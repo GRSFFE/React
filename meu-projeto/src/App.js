97bba16d2a5b21f4b829fd06f957de665237ff73
Germanno Resende <germannoresende@gmail.com>
2023-04-20 20:19:13 -0300

    Utilizando o JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 
 function App() {
+  const name = "Matheus";
+
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = "https://via.placeholder.com/150";
+
   return (
     <div className="App">
-      <h1>Olá, react</h1>
-      <p>Meu primeiro App</p>
+      <h1>Alterando o JSX</h1>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem" />
     </div>
   );
 }

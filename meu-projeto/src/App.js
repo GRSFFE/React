c8665f70106e8fe7b4e1bd31f0911796d7be1496
Germanno Resende <germannoresende@gmail.com>
2023-04-28 18:45:24 -0300

    Adiciona Props.

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,24 +1,21 @@
 import "./App.css";
-import HellowWorld from "./components/HelloWorld";
+import SayMyName from "./components/sayMyName";
+import Pessoa from "./components/Pessoa";
 
 function App() {
-  const name = "Matheus";
-
-  const newName = name.toUpperCase();
-
-  function sum(a, b) {
-    return a + b;
-  }
-
-  const url = "https://via.placeholder.com/150";
+  const nome = "Maria";
 
   return (
     <div className="App">
-      <h1>Alterando o JSX</h1>
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="Minha imagem" />
-      <HellowWorld />
+      <SayMyName nome="Jorge" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa
+        nome="Rodrigo"
+        idade="28"
+        profissao="Programador"
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   );
 }

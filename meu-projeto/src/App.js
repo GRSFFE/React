cfff2dc916bbcc101185d3c1a7b8a5b2cdc6f069
Germanno Resende <germannoresende@gmail.com>
2023-05-04 10:13:25 -0300

    Testando Eventos no React

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,27 +1,14 @@
+/* eslint-disable react/jsx-no-undef */
 import "./App.css";
-import SayMyName from "./components/sayMyName";
-import Pessoa from "./components/Pessoa";
-import Frase from "./components/Frase";
-import List from "./components/List";
+import Evento from "./components/Evento";
 
 function App() {
-  const nome = "Maria";
-
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="Jorge" />
-      <SayMyName nome="João" />
-      <SayMyName nome={nome} />
-      <Pessoa
-        nome="Rodrigo"
-        idade="28"
-        profissao="Programador"
-        foto="https://via.placeholder.com/150"
-      />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
     </div>
   );
 }
